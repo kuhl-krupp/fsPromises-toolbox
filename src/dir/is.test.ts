11080b243a4bbac66cb307d4720e1ed47dad7dbb
Tjark Kühl <tjark-kuehl@hotmail.com>
2020-10-31 00:59:20 +0100

    Update packages & fix tests

--- src/dir/is.test.ts
+++ src/dir/is.test.ts
@@ -1,4 +1,4 @@
-import { join } from 'path';
+import path from 'path';
 import { isDir } from './is';
 
 const TEST_DIR = process.env.TEST_DIR_PATH ?? '';
@@ -6,35 +6,35 @@ const TEST_DIR = process.env.TEST_DIR_PATH ?? '';
 describe('isFile', () => {
     describe('Check if file is a directory', () => {
         it('Should return false', async () => {
-            const res = await isDir(join(TEST_DIR, 'lorem', 'ipsum', 'test.txt'));
+            const res = await isDir(path.join(TEST_DIR, 'lorem', 'ipsum', 'test.txt'));
             expect(res).toBe(false);
         });
     });
 
     describe('Check if file is not a directory', () => {
         it('Should return true', async () => {
-            const res = !(await isDir(join(TEST_DIR, 'lorem', 'ipsum', 'test.txt')));
+            const res = !(await isDir(path.join(TEST_DIR, 'lorem', 'ipsum', 'test.txt')));
             expect(res).toBe(true);
         });
     });
 
     describe('Check if directory is a directory', () => {
         it('Should return true', async () => {
-            const res = await isDir(join(TEST_DIR, 'lorem'));
+            const res = await isDir(path.join(TEST_DIR, 'lorem'));
             expect(res).toBe(true);
         });
     });
 
     describe('Check if deep directory is a directory', () => {
         it('Should return true', async () => {
-            const res = await isDir(join(TEST_DIR, 'lorem', 'ipsum'));
+            const res = await isDir(path.join(TEST_DIR, 'lorem', 'ipsum'));
             expect(res).toBe(true);
         });
     });
 
     describe('Check if not existing path returns false', () => {
         it('Should return false', async () => {
-            const res = await isDir(join(TEST_DIR, Math.random().toString()));
+            const res = await isDir(path.join(TEST_DIR, Math.random().toString()));
             expect(res).toBe(false);
         });
     });
--- src/file/is.test.ts
+++ src/file/is.test.ts
@@ -1,4 +1,4 @@
-import { join } from 'path';
+import path from 'path';
 import { isFile } from './is';
 
 const TEST_DIR = process.env.TEST_DIR_PATH ?? '';
@@ -6,28 +6,28 @@ const TEST_DIR = process.env.TEST_DIR_PATH ?? '';
 describe('isFile', () => {
     describe('Check if directory is a file', () => {
         it('Should return false', async () => {
-            const res = await isFile(join(TEST_DIR, 'lorem'));
+            const res = await isFile(path.join(TEST_DIR, 'lorem'));
             expect(res).toBe(false);
         });
     });
 
     describe('Check if directory is not a file', () => {
         it('Should return true', async () => {
-            const res = !(await isFile(join(TEST_DIR, 'lorem')));
+            const res = !(await isFile(path.join(TEST_DIR, 'lorem')));
             expect(res).toBe(true);
         });
     });
 
     describe('Check if file is a file', () => {
         it('Should return true', async () => {
-            const res = await isFile(join(TEST_DIR, 'lorem', 'ipsum', 'test.txt'));
+            const res = await isFile(path.join(TEST_DIR, 'lorem', 'ipsum', 'test.txt'));
             expect(res).toBe(true);
         });
     });
 
     describe('Check if not existing path returns false', () => {
         it('Should return false', async () => {
-            const res = await isFile(join(TEST_DIR, Math.random().toString()));
+            const res = await isFile(path.join(TEST_DIR, Math.random().toString()));
             expect(res).toBe(false);
         });
     });
