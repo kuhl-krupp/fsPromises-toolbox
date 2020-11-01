8a83e9d6e424ede7a217075b93b0ddf8f0dfbd35
Tjark Kühl <tjark-kuehl@hotmail.com>
2020-11-01 20:42:34 +0100

    Added global fs

--- rollup.config.js
+++ rollup.config.js
@@ -6,22 +6,28 @@ import { terser as terserPlugin } from 'rollup-plugin-terser';
 import pkg from './package.json';
 
 const outputDir = 'dist';
+const globals = {
+    fs: 'fs'
+};
 
 export default {
     input: 'src/index.ts',
     output: [
         {
             file: `${outputDir}/index.js`,
-            format: 'cjs'
+            format: 'cjs',
+            globals
         },
         {
             file: `${outputDir}/index.es.js`,
-            format: 'esm'
+            format: 'esm',
+            globals
         },
         {
             name: 'OpenIcecat',
             file: `${outputDir}/index.umd.js`,
-            format: 'umd'
+            format: 'umd',
+            globals
         }
     ],
     external: [...Object.keys(pkg.dependencies || {}), ...Object.keys(pkg.peerDependencies || {})],
