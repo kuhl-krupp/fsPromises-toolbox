cf4a2a98fa1f772c4ca4d0f1c524c2e87035e80d
Tjark Kühl <tjark-kuehl@hotmail.com>
2020-11-01 22:36:45 +0100

    Rename function arguments

--- src/dir/is.ts
+++ src/dir/is.ts
@@ -5,12 +5,12 @@ import { exists } from '../_helpers/exists';
  * Check if given path is a valid directory
  *
  * @export
- * @param {string} path DirectoryPath
+ * @param {string} dirPath DirectoryPath
  * @returns {Promise<boolean>} true: if path is a directory, false: if path doesn't exist or it isn't a directory
  */
-export async function isDir(path: string): Promise<boolean> {
-    if (await exists(path)) {
-        const stat = await pfs.lstat(path);
+export async function isDir(dirPath: string): Promise<boolean> {
+    if (await exists(dirPath)) {
+        const stat = await pfs.lstat(dirPath);
         return stat.isDirectory();
     }
     return false;
--- src/file/is.ts
+++ src/file/is.ts
@@ -5,12 +5,12 @@ import { exists } from '../_helpers/exists';
  * Check if given path is a valid file
  *
  * @export
- * @param {string} path FilePath
+ * @param {string} filePath FilePath
  * @returns {Promise<boolean>} true: if path is a file, false: if path doesn't exist or it isn't a file
  */
-export async function isFile(path: string): Promise<boolean> {
-    if (await exists(path)) {
-        const stat = await pfs.lstat(path);
+export async function isFile(filePath: string): Promise<boolean> {
+    if (await exists(filePath)) {
+        const stat = await pfs.lstat(filePath);
         return stat.isFile();
     }
     return false;
