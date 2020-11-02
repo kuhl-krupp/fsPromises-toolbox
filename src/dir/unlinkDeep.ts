db4b27df042faf8ed57cb38e693b80a68abf740a
Tjark Kühl <tjark-kuehl@hotmail.com>
2020-11-02 11:31:00 +0100

    Added unlinkDeep & Update node to lts

---
+++ src/dir/unlinkDeep.ts
@@ -0,0 +1,40 @@
+import { promises as pfs } from 'fs';
+import path from 'path';
+import { readDeep } from './readDeep';
+import { isDir } from './is';
+
+/**
+ *
+ * It will return undefined when the dirPath is invalid
+ *
+ * @export
+ * @param {string} dirPath DirectoryPath
+ * @returns {Promise<true|undefined>}
+ */
+export async function unlinkDeep(dirPath: string): Promise<true | undefined> {
+    // Check if given path is a valid directory
+    if (!(await isDir(dirPath))) {
+        return undefined;
+    }
+
+    // Get deep list of files and iterate
+    let fileDir;
+    for await (const file of await readDeep(dirPath)) {
+        // Unlink each file
+        await pfs.unlink(file);
+
+        const newFileDir = path.dirname(file);
+        if (fileDir === undefined) {
+            fileDir = newFileDir;
+        } else if (fileDir !== newFileDir) {
+            // Remove dir
+            await pfs.rmdir(fileDir);
+            fileDir = newFileDir;
+        }
+    }
+
+    // Remove dirPath
+    await pfs.rmdir(dirPath);
+
+    return true;
+}
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
+/* eslint-disable import/no-cycle */
 // Dir
 import { isDir } from './dir/is';
 import { readDeep } from './dir/readDeep';
+import { unlinkDeep } from './dir/unlinkDeep';
 
 // File
 import { isFile } from './file/is';
@@ -10,7 +12,9 @@ export {
     // Dir
     isDir,
     readDeep,
+    unlinkDeep,
     // File
     isFile,
     fileStats
 };
+/* eslint-enable import/no-cycle */
